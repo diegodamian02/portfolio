aef737b7c6ee66c2f6dd4b21e9fa7ad32f2a6ccb
Diego Damian <dad405@scarletmail.rutgers.edu>
2025-02-23 23:36:13 -0500

    spotify 3 UPDATE

--- server/server.js
+++ server/server.js
@@ -2,35 +2,19 @@ import express from "express";
 import cors from "cors";
 import axios from "axios";
 import dotenv from "dotenv";
-import querystring from "querystring";
+
 
 dotenv.config();
 const app = express();
-app.use(cors({
-    origin: "http://localhost:5173",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true
-}));
+app.use(cors());
 
 //Spotify API Endpoint
 const CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
 const CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
-const REDIRECT_URI = process.env.REDIRECT_URI;
+const TOKEN_ENDPOINT = "https://accounts.spotify.com/api/token";
+let accessToken = "";
 
-app.get("/login", (req, res) => {
-    const scope = "user-read-private user-read-email user-top-read";
-    const authUrl = "https://accounts.spotify.com/authorize?" + querystring.stringify({
-        response_type: "code",
-        client_id: CLIENT_ID,
-        scope: scope,
-        redirect_uri: REDIRECT_URI,
-        code_challenge_method: "S256",
-        code_challenge: "challenge_value",
-
-    });
-    res.redirect(authUrl);
-})
-
+/* Callback discarded :( maybe for some other time
 app.get("/callback", async (req, res) => {
     const code = req.query.code || null;
     try {
@@ -54,8 +38,7 @@ app.get("/callback", async (req, res) => {
     }
 });
 
-
-/*Function to fetch Spotify data
+// Function to fetch Spotify data
 async function fetchSpotifyData(endpoint) {
     try{
         const response = await axios.get(`${SPOTIFY_API_URL}/${endpoint}`, {
@@ -71,35 +54,31 @@ async function fetchSpotifyData(endpoint) {
 }
 */
 
-// Fetch user profile data
-
-app.get("/api/spotify/profile", async (req, res) => {
-    const access_token = req.query.access_token;
-
-    if (!access_token) {
-        return res.status(400).json({ error: "Access token required"});
-    }
-
+// Function to get a new access token
+async function getSpotifyAccessToken() {
     try {
-        const response = await axios.get("https://api.spotify.com/v1/me", {
-            headers: {Authorization: `Bearer ${access_token}`},
-        });
-        res.json(response.data);
+        const response = await axios.post(
+            TOKEN_ENDPOINT,
+            new URLSearchParams({
+                grat_type: "client_credentials",
+                client_id: CLIENT_ID,
+                client_secret: CLIENT_SECRET,
+            }),
+            { headers: { "Content-Type": "application/x-www-form-urlencoded"}}
+        );
 
-    } catch (error){
-        console.error("Error fetching Spotify profiles:", error.response?.data || error.message)
-        res.status(500).json({ error: "Failed to fetch Spotify profile"});
+        accessToken = response.data.access_token;
+        console.log("New Spotify Access Token:", accessToken);
+    } catch (error) {
+        console.error("Error getting Spotify access token:", error.response.data || error.message);
     }
-});
+}
+// Get a new token when the server starts
+getSpotifyAccessToken();
 
 
 // API Endpoint for Top 5 Tracks - Fetching
 app.get("/api/spotify/top-tracks", async (req, res) => {
-    const access_token = req.query.access_token;
-    if (!access_token){
-        return res.status(400).json({error: "Access token required"});
-    }
-
     try{
         const reponse = await axios.get("https://api.spotify.com/v1/me/top/tracks?limit=5", {
             headers: {Authorization: `Bearer ${access_token}`},
@@ -114,11 +93,6 @@ app.get("/api/spotify/top-tracks", async (req, res) => {
 // API Endpoint for Top 5 Artist
 
 app.get("/api/spotify/top-artists", async (req, res) => {
-    const access_token = req.query.access_token;
-    if (!access_token) {
-        return res.status(400).json({ error: "Access token required" });
-    }
-
     try {
         const response = await axios.get("https://api.spotify.com/v1/me/top/artists?limit=5", {
             headers: { Authorization: `Bearer ${access_token}` },
